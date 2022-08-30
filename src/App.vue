0a1f20440589dffbf05c744cc8c8b7dc7b18f201
EduardBru <ebrudomingo@gmail.com>
2022-08-30 21:45:58 +0200

    first commit malo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <section>
+    <router-view class="app-main" /> <!-- your routes will load inside of these tags -->    
+  </section>
 </template>
+ 
+<script >
+import userStore from '@/store/user';
+import {mapState, mapActions} from 'pinia';
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+export default {
+  name: 'App',
+  computed: {
+    ...mapState(userStore, )
+  },
+  
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+ 
+const router = useRouter()
+const userStore = useUserStore()
+const { user } = storeToRefs(userStore)
+ 
+onMounted(async () => {
+  try {
+    await userStore.fetchUser() // here we call fetch user
+    if (!user.value) {
+      // redirect them to logout if the user is not there
+      router.push({ path: '/auth' });
+    } else {
+      // continue to dashboard
+      router.push({ path: '/' });
+    }
+  } catch (e) {
+    console.log(e)
+  }
+})
+</script>
+ 
---
+++ src/views/AuthView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1> authvue</h1>
+</template>
