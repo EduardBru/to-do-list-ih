d9f89ebd33596685a34942e8a29dfb71e7a0b7fa
EduardBru <ebrudomingo@gmail.com>
2022-09-07 00:06:19 +0200

    update creado

--- src/components/TaskJobs.vue
+++ src/components/TaskJobs.vue
@@ -1,13 +1,49 @@
-<!-- hay que crear el form, metiendo el user_id que tengamos en user
+<!-- eslint-disable vuejs-accessibility/form-control-has-label -->
+<!-- eslint-disable vuejs-accessibility/label-has-for
+  hay que crear el form, metiendo el user_id que tengamos en user
   (habra que ver como hacemos eso xq el data no me lo permite, quiza un computed o
    quiza modificandolo en el propio código)
    Tambien hay que revisar que el v-for actualice, cosa que ahora no hace.
    Tambien habra que meter los metodos para hacer el update, no lo veo super dificl la verdad
+   esto es un componente que carga las tareas, habra otro que las represente
 -->
 <template>
     <div>task components incoming</div>
-    <div v-for="item in tasks" :key="item.id" > {{item}}</div>
+    <div v-for="item in tasks" :key="item.id" >
+      <form  @submit.prevent="handleUpdateTask(item.id)" >
+      Task:
+      <input v-model="item.title" placeholder="item.title" class="task_data">
+      <input v-model.number="item.estimate" placeholder="Insert" type="number" class="task_data">
+      <select v-model="item.state" placeholder="item.state" class="task_data">
+        <option v-for="state in states" v-bind:value="state" :key="state">
+        {{ state }}
+        </option>
+      </select>
+      Plan Estimate:  {{item.estimate}}
+      Status:  {{item.state}}
+      <input type="submit" value="Update Task">
+    </form>
+      <button @click="handleDeleteTask(item.id)">DeleteTask </button>
+    </div>
     <button @click="handleCreateTask">CreateTask </button>
+    <form  @submit.prevent="handleCreateTask" >
+      <div>
+        <label for="title">Title</label>
+        <input name="title" v-model="taskObject.title" placeholder="Insert your task name here">
+      </div>
+      <div>
+        <label for="estimate">estimation</label>
+        <input name="estimate" v-model.number="taskObject.estimate"
+        placeholder="Insert" type="number">
+      </div>
+      <select v-model="taskObject.state">
+        <option v-for="item in states" v-bind:value="item" :key="item">
+        {{ item }}
+        </option>
+      </select>
+      <span>Seleccionado: {{ taskObject.state }}</span>
+      <input type="submit" value="Create Task">
+    </form>
 </template>
 
 <script>
@@ -15,36 +51,62 @@ import { mapState, mapActions } from 'pinia';
 import userStore from '@/store/user';
 import taskStore from '@/store/task';
 
+const defaultData = {
+  title: '',
+  estimate: 0,
+  state: 'To Do',
+  user_id: '',
+};
 export default {
   name: 'TasksJobs',
   components: { },
   data() {
     return {
+      selectedState: '',
       taskObject: {
-        title: 'pepe',
-        estimate: 2,
+        title: '',
+        estimate: 0,
         state: 'To Do',
-        user_id: 'db7367e9-bf26-49cc-8ffd-8a7bdcc9601b',
+        user_id: '',
+        subtasks: [],
       },
     };
   },
   computed: {
-    ...mapState(taskStore, ['tasks']),
+    ...mapState(taskStore, ['tasks', 'states']),
     ...mapState(userStore, ['user']),
   },
   methods: {
-    ...mapActions(taskStore, ['fetchTasks', 'createTask']),
+    ...mapActions(taskStore, ['fetchTasks', 'createTask', 'deleteTask', 'fetchStates', 'updateTask']),
     handleCreateTask() {
+      this.taskObject.user_id = this.user.id;
       this.createTask(this.taskObject);
+      this.taskObject = { ...defaultData };
+    },
+    handleDeleteTask(taskId) {
+      console.log(taskId);
+      this.deleteTask(taskId);
+    },
+    handleUpdateTask(taskId) {
+      console.log(taskId);
+      this.updateTask(taskId);
     },
   },
   async created() {
     try {
       await this.fetchTasks(); // here we call fetch user
       console.log(this.tasks);
+      await this.fetchStates(); // here we call fetch user
     } catch (e) {
       console.log(e);
     }
   },
 };
 </script>
+
+<style>
+  .task_data{
+    border: 0px;
+    width: 200px;
+  }
+</style>
--- src/store/task.js
+++ src/store/task.js
@@ -5,21 +5,56 @@ export default defineStore('tasks', {
   state: () => ({
     taskCreated: null,
     tasks: [],
+    states: [],
   }),
   actions: {
+    async fetchStates() {
+      const { data } = await supabase
+        .from('user_config')
+        .select('states');
+      this.states = data[0].states;
+    },
     async fetchTasks() {
       const { data } = await supabase
         .from('tasks')
         .select('*')
-        .order('id', { ascending: false });
+        .order('id', { ascending: true });
       this.tasks = data;
+      // si tiene subtasks, recuperarlas
     },
     async createTask(task) {
-      const { taskReturned, error } = await supabase
+      const { data, error } = await supabase
         .from('tasks')
         .insert(task);
       if (error) throw error;
-      else this.taskCreated = taskReturned;
+      else {
+        this.tasks.push(data[0]);
+      }
+    },
+    async updateTask(taskId) {
+      const updatedTaskId = this.tasks.map((item) => item.id).indexOf(taskId);
+      const updatedTask = this.tasks[updatedTaskId];
+      const { data, error } = await supabase
+        .from('tasks')
+        // eslint-disable-next-line max-len
+        .update({ title: updatedTask.title, estimate: updatedTask.estimate, state: updatedTask.state })
+        .match({ id: taskId });
+      if (error) throw error;
+      else {
+        this.tasks[updatedTaskId] = { ...data[0] };
+      }
+    },
+    async deleteTask(taskId) {
+      const { error } = await supabase
+        .from('tasks')
+        .delete()
+        .match({ id: taskId });
+      if (error) throw error;
+      else {
+        const removeIndex = this.tasks.map((item) => item.id).indexOf(taskId);
+        this.tasks.splice(removeIndex, 1);
+        console.log(removeIndex);
+      }
     },
   },
 });
